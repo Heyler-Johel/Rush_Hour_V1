c6e2caed63f5c2c4216f679519b805dbb9ef0b33
Greivin Calderón Duran <johelmora@gmail.com>
2022-04-06 16:31:06 -0600

    Add Memento

---
+++ Rush_Hour_V1/src/Memento/Caretaker.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Memento;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author johel
+ */
+public class Caretaker {
+    
+    private final ArrayList<Memento> states = new ArrayList<>();
+    
+    public void addMemento(Memento memento){
+        states.add(memento);
+    }
+    
+    public Memento getMemento(int index){
+        return states.get(index);
+    }
+}
